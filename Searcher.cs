e168b949b0ee9288a80950fbd0dd8c89d2747b34
ollana <olganasirov@gmail.com>
2017-01-12 20:37:09 +0200

    Searcher

---
+++ Searcher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRProject
+{
+    class Searcher
+    {
+
+        public List<string> AutoComplete(string word)
+        {
+            List<string> complete = new List<string>();
+
+            return complete;
+        }
+
+        public List<string> Search(string query)
+        {
+            List<string> relevant_docs = new List<string>();
+
+            return relevant_docs;
+
+        }
+
+    }
+}
