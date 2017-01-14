ce169098d59768057f87e33ff5cee79cf2650330
azizonen <azizonen@gmail.com>
2017-01-14 20:29:25 +0200

    new ranker

---
+++ Ranker/BM25.cs
@@ -0,0 +1,55 @@
+﻿using IRProject.Terms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRProject.Ranker
+{
+    class BM25
+    {
+        double _k1, _k2, _b, _K;
+            int _ri, _R;
+        public BM25(string query, double k1, double k2, double b, int R, int ri)
+        {
+            _k1 = k1;
+            _k2 = k2;
+            _b = b;
+            _ri = ri;
+            _R = R;
+        }
+
+        /// <summary>
+        /// this method clcluate the BM25 scoure for a list of documents and a query.
+        /// </summary>
+        /// <param name="Document"></param>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        public double Score(Tuple<Document,int> Document, string query, int DocumentFrequency)
+        {
+            string[] Q = query.Split(' ');//בעיה... אם הטרם הוא עם רווחים... צריך לבדוק עם הפרסר לפני
+            double sum = 0;
+            foreach (var term in Q)
+            {
+                //sum += ScoreOne(Document.Item1.DocumentLength,_AverageDocLength,DocumentFrequency,_NumberOfDocuments,Document.Item2
+            }
+            return sum;
+        }
+
+        /// <summary>
+        /// BN25 inner sigma loop
+        /// </summary>
+        /// <param name="Document"></param>
+        /// <param name="term"></param>
+        /// <returns></returns>
+        public double ScoreOne(double _dl, double _avgdl, double _ni, double _N, double _fi, double _qfi)
+        {
+            _K = _k1 * ((_b * (_dl /_avgdl)) + (1 - _b));
+            double toLog = ((_ri + 0.5) / (_R - _ri + 0.5)) / ((_ni - _ri + 0.5) / (_N - _ni - _R + _ri + 0.5));
+            double docK = ((_k1 + 1) * _fi) / (_K + _fi);
+            double queK = ((_k2 + 1) * _qfi) / (_k2 + _qfi);
+            return (Math.Log(toLog) * docK * queK);
+        }
+    }
+}
---
+++ Ranker/Jaccard.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace IRProject.Ranker
+{
+    class Jaccard
+    {
+        Dictionary<string,List<int>> DocTermsList;
+        public Jaccard(string PostingPath)
+        {
+            DocTermsList = new Dictionary<string, List<int>>();
+            using (System.IO.StreamReader sr = new System.IO.StreamReader(PostingPath))
+            {
+                int termCounter = 0;
+                string line = sr.ReadLine();
+                while (line!=null)
+                {
+                    string[] docs = line.Split('|');
+                    foreach (string s in docs)
+                    {
+                        string doc = s.Split(' ')[0];
+                        if (!DocTermsList.ContainsKey(doc))
+                            DocTermsList.Add(doc, new List<int>());
+                        DocTermsList[doc].Add(termCounter); 
+                    }
+                    line = sr.ReadLine();
+                    termCounter++;
+                }
+            }
+        }
+
+        public void JaccardToFile(string fileName,double rank)
+        {
+            if (!System.IO.File.Exists(fileName))
+                System.IO.File.Create(fileName).Close();
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter(fileName))
+            {
+                foreach (string doc1 in DocTermsList.Keys)
+                { 
+                    foreach (string doc2 in DocTermsList.Keys)
+                    {
+                        double ans = Calc(DocTermsList[doc1], DocTermsList[doc2]);
+                        if (ans >= rank)
+                            sw.Write(doc2+" "+ans+"|");
+                    }
+                    sw.Write(sw.NewLine);
+                }
+            }
+                
+        }
+        public double Calc(HashSet<int> hs1, HashSet<int> hs2)
+        {
+            return hs1.Intersect(hs2).Count() / (double)hs1.Union(hs2).Count();
+        }
+
+        public double Calc(List<int> ls1, List<int> ls2)
+        {
+            HashSet<int> hs1 = new HashSet<int>(ls1);
+            HashSet<int> hs2 = new HashSet<int>(ls2);
+            return Calc(hs1, hs2);
+        }
+    }
+}
