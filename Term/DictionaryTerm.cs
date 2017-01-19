aa3d60f3cd9661ee8ba561409e0137de9fd06247
ollana <olganasirov@gmail.com>
2017-01-19 19:04:59 +0200

    position parser

--- Term/DictionaryTerm.cs
+++ Term/DictionaryTerm.cs
@@ -11,16 +11,24 @@ namespace IRProject
         private int m_tf;
         private int m_df;
         private int m_line;
+        private long m_start, m_end;
         public int TF { get { return m_tf; } }
         public int DF { get { return m_df; } }
         public int LineNumber { get { return m_line; } }
+        public long StartPosition { get { return m_start; } }
+        public long EndPosition { get { return m_end; } }
 
-        public DictionaryTerm(string term, int tf, int df, int line)
+
+
+        public DictionaryTerm(string term, int tf, int df, int line,long start,long end)
         {
             m_term = term;
             m_tf = tf;
             m_df = df;
             m_line = line;
+            m_start = start;
+            m_end=end;
+
         }
     }
 }
